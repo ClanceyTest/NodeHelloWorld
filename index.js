b1c8a8a0b249fb0eb7a7c0419db11224ee1d12bb
James Clancey <james.clancey@gmail.com>
2018-04-17 15:53:28 -0800

    change

--- index.js
+++ index.js
@@ -10,4 +10,4 @@ var server = http.createServer(function(request, response) {
 var port = process.env.PORT || 1337;
 server.listen(port);
 
-console.log("Server running at test 2 http://localhost:%d", port);
+console.log("Server running at test push http://localhost:%d", port);
